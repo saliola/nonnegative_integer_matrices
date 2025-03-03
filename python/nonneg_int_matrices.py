33a736d700195407c9a6b477e6d2081f22d06d09
Franco Saliola <saliola@gmail.com>
2025-03-03 10:18:47 -0500

    cleanup

--- python/nonneg_int_matrices.py
+++ python/nonneg_int_matrices.py
@@ -1,11 +1,9 @@
-def nonneg_int_vectors_iter(n, length, ceiling):
+def nonneg_int_vectors_iter(n, ceiling):
     r"""
-    Vectors of nonnegative integers
-    of prescribed `length` and sum `n`
+    Vectors of nonnegative integers that sum to `n`
     whose parts are bounded according to `ceiling`.
 
-    >>> iter = nonneg_int_vectors_iter(5, 3, [3, 1, 5])
-    >>> for v in iter:
+    >>> for v in nonneg_int_vectors_iter(5, [3, 1, 5]):
     ...     print(v)
     [0, 0, 5]
     [0, 1, 4]
@@ -16,24 +14,20 @@ def nonneg_int_vectors_iter(n, length, ceiling):
     [3, 0, 2]
     [3, 1, 1]
     """
-    assert length == len(ceiling)
-
-    if length == 1:
+    if len(ceiling) == 1:
         if n <= ceiling[0]:
             yield [n]
     else:
         for first_entry in range(min(ceiling[0], n) + 1):
-            for vec in nonneg_int_vectors_iter(n - first_entry, length - 1, ceiling[1:]):
+            for vec in nonneg_int_vectors_iter(n - first_entry, ceiling[1:]):
                 yield [first_entry] + vec
 
 
 def nonneg_int_matrices_iter(row_sums, col_sums):
     r"""
-    Matrices of nonnegative integers
-    with prescribed row and column sums.
+    Matrices of nonnegative integers with prescribed row and column sums.
 
-    >>> iter = nonneg_int_matrices_iter([3,2,2], [2,5])
-    >>> for m in iter:
+    >>> for m in nonneg_int_matrices_iter([3,2,2], [2,5]):
     ...     print(m)
     [[0, 3], [0, 2], [2, 0]]
     [[0, 3], [1, 1], [1, 1]]
@@ -48,12 +42,8 @@ def nonneg_int_matrices_iter(row_sums, col_sums):
     if len(row_sums) == 1:
         yield [col_sums]
     else:
-        for first_row in nonneg_int_vectors_iter(row_sums[0],
-                                                 length=len(col_sums),
-                                                 ceiling=col_sums):
-            truncated_row_sums = row_sums[1:]
-            truncated_col_sums = [col_sums[i] - first_row[i] for i in range(len(col_sums))]
-            for mat in nonneg_int_matrices_iter(truncated_row_sums, truncated_col_sums):
+        for first_row in nonneg_int_vectors_iter(row_sums[0], ceiling=col_sums):
+            for mat in nonneg_int_matrices_iter(row_sums[1:], [ci - fi for (ci, fi) in zip(col_sums, first_row)]):
                 yield [first_row] + mat
 
 
